f852b456527501983f4e37b977c11077a4126136
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-12-27 11:28:59 -0300

    Update deprecated bcrypt-nodejs to bcrypt

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,11 @@
+const saltRounds = parseInt(process.env.SALT_ROUNDS) || 10;
+
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
+  const hash = bcrypt.hashSync(password, saltRounds);
     db.transaction(trx => {
       trx.insert({
         hash: hash,
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
